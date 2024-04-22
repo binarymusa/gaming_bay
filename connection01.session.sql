31cfa05b47f5e20cef938486ec6a88ce0472caf8
binarymusa <finicky9819@gmail.com>
2024-04-22 13:04:23 +0300

    minor mods on Admin, base pages

--- connection01.session.sql
+++ connection01.session.sql
@@ -2,18 +2,19 @@
 /*
 
 USE <DB.NMAE>
-DELETE FROM <db.name>  or filter using (DELETE FROM <db.name> WHERE <column.name>=<name> IN (<column_id>)
-SELECT * FROM <db.nmae> or SELECT * FROM <db.name> WHERE <column.name>= <name>
+DELETE FROM <table_nmae>  or filter using (DELETE FROM <table_nmae> WHERE <column.name>=<name> IN (<column_id>)
+SELECT * FROM <table.nmae> or SELECT * FROM <table.name> WHERE <column.name>= <name>
 DROP TABLE table_name;
 DROP TABLE IF EXISTS table_name
-INSERT INTO <db_nmae (<column_name>) VALUES ('<valuea>'), ('v<value_b>')
+INSERT INTO <table_nmae> (<column_name>) VALUES ('<valuea>'), ('v<value_b>')
 
 -- updating a value to a column in a tabble
 UPDATE <table_name> SET <column_name> = 'id_of_normal_user_role' WHERE id = <id>;
+CREATE DATABASE 
 
 */
 
-SELECT * FROM user
+DELETE FROM user WHERE id=7
 
 
 
